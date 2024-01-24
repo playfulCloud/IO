47dfacb740de7cb700ee58d5d1bf880581bc72fa
Łukasz Mędrek <lukasz@lukaszm.xyz>
2024-01-24 11:54:52 +0100

    wip: Add register test

--- mondryptaszeknasosnie/src/test/java/org/ptaszek/Controller/Fixture.java
+++ mondryptaszeknasosnie/src/test/java/org/ptaszek/Controller/Fixture.java
@@ -1,15 +1,45 @@
 package org.ptaszek.Controller;
 
+import java.sql.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+import org.ptaszek.Model.RegistrationData;
+import org.ptaszek.Model.UserData;
+
 public class Fixture {
 	public int a;
 
+	    public String name;
+	    public String surname;
+	    public String PESEL;
+	    public String IDNumber;
+	    public String mailAdress;
+	    public java.util.Date birthDate;
+
+	    private String data_login;
+	    private String data_password;
+
 	public Fixture() {}
 
-	public void setA(int v) {
-		a = v;
-	}
+	public void setName(String v) { name = v; }
+	public void setSurname(String v) { surname = v; }
+	public void setPESEL(String v) { PESEL = v; }
+	public void setIDNumber(String v) { IDNumber = v; }
+	public void setMailAdress(String v) { mailAdress = v; }
+	public void setBirthDate(String v) { try { birthDate = new SimpleDateFormat("yyyy-mm/dd").parse(v); } catch(Exception e){} } // .-.
 
-	public int a_add() {
-		return a + 1;
+
+	public void setLogin(String v) { data_login = v; }
+	public void setPassword(String v) { data_password = v; }
+	
+	public int register() {
+		var ud = new UserData(data_login, data_password);
+		var rd = new RegistrationData(name, surname, PESEL, IDNumber, mailAdress, birthDate, ud);
+
+		var a = new Application();
+		var response = a.register(rd);
+
+		return response.getCode();
 	}
 }

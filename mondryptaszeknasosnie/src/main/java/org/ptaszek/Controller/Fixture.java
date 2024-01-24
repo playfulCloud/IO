81bcdf1eaae5e49a9003a60fbc485297a6e60bbe
Łukasz Mędrek <lukasz@lukaszm.xyz>
2024-01-24 11:29:49 +0100

    Add working test example

---
+++ mondryptaszeknasosnie/src/main/java/org/ptaszek/Controller/Fixture.java
@@ -0,0 +1,16 @@
+package org.ptaszek.Controller;
+
+public class Fixture {
+	public int a;
+
+	public Fixture() {}
+
+	public void setA(int v) {
+		a = v;
+	}
+
+	public int a_add() {
+		return a + 1;
+	}
+}
+
---
+++ mondryptaszeknasosnie/src/test/java/org/ptaszek/Controller/Fixture.java
@@ -0,0 +1,15 @@
+package org.ptaszek.Controller;
+
+public class Fixture {
+	public int a;
+
+	public Fixture() {}
+
+	public void setA(int v) {
+		a = v;
+	}
+
+	public int a_add() {
+		return a + 1;
+	}
+}
